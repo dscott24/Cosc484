3054db699fcc23fb46452f4a235e00f5730d5162
jrodriquez <jrodri30@students.towson.edu>
2019-03-19 16:42:40 -0400

    created database tables and linked them to admin site page

--- GroupProject/MessageApp/MessageApp/models.py
+++ GroupProject/MessageApp/MessageApp/models.py
@@ -3,6 +3,31 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import PermissionsMixin
 from django.utils import timezone
+import uuid
+
+class User(models.Model):
+    user_id = models.UUIDField(primary_key=True,editable=False)
+    f_name = models.CharField(max_length=30)
+    l_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    username = models.CharField(max_length=40, unique=True)
+    profile_img = models.ImageField()
+
+class Thread(models.Model):
+    name =  models.CharField(max_length=40)
+    thread_ID = models.UUIDField(primary_key=True,editable=False)
 
 class Message(models.Model):
-    text = models.CharField('String', max_legnt)
+    text = models.TextField()
+    message_ID = models.UUIDField(primary_key=True, editable=False)
+    user_ID = models.ForeignKey(User, on_delete=models.PROTECT)
+    image = models.ImageField()
+    thread_ID= models.ForeignKey(Thread, on_delete=models.PROTECT)
+    date_time = models.DateTimeField()
+
+class Member(models.Model):
+    user_ID = models.ForeignKey(User, on_delete=models.PROTECT)
+    thread_ID = models.ForeignKey(Thread, on_delete=models.PROTECT)
+
+
+

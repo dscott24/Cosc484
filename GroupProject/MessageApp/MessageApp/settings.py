a6e85bafe4f5bec37b3627acd957664d2b3c45eb
aakwei1 <43798592+aakwei1@users.noreply.github.com>
2019-04-09 19:56:09 -0400

    Revert "Merge branch 'master' of https://github.com/dscott24/Cosc484"
    This reverts commit 3714c0f9d0b915e48e56b6f8471895b28d67397f, reversing
    changes made to 8de7db6dd09e897309f74a5addeb6c6d53affdbc.

--- GroupProject/MessageApp/MessageApp/settings.py
+++ GroupProject/MessageApp/MessageApp/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    #own apps
-    'pages'      
+                  
 ]
 
 #AUTH_USER_MODEL = 'MessageApp.User'
